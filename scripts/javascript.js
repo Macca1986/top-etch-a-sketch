ae192c7642fa1dfc8246457e82a6601d4f34f321
Macca1986 <matthew_mack86@hotmail.com>
2025-02-13 18:27:23 +0400

    Add square gird to page with javascript
    Used javascript and flexbox to add a 4x4 grid of squares to the page.
    Event listener requires debugging to work as intended, additional
    work on the squares is required for them to be responsive.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,1 +1,29 @@
 console.log("hello world");
+
+const container = document.querySelector("#container");
+console.log(container);
+
+for (let i = 0; i < 16; i++) {
+    const squareDiv = document.createElement("div");
+    squareDiv.setAttribute("class","square");
+    container.appendChild(squareDiv);
+    console.log(i);
+}
+
+// event listeners
+const squares = document.querySelectorAll(".square");
+console.log(squares);
+
+// for (let i = 0; i < squares.length; i++) {
+//     const currentSquare = squares[i];
+//     currentSquare.addEventListener("onclick", () => {
+//         currentSquare.setAttribute("class", "black-square");
+//         console.log("square " + i + " was clicked");
+//     });
+//     console.log("added event listener to square " + i);
+// }
+
+squares[0].addEventListener("onclick", (event) => {
+    event.target.setAttribute("class", "black-square");
+    console.log("square 0 was clicked");
+});
