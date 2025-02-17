af231de483d78acf13a995a7d83af177f9900ded
Macca1986 <matthew_mack86@hotmail.com>
2025-02-17 17:51:13 +0400

    Add squares and mouseover event listeners
    Created a 16x16 grid of squares using javascript and added event
    listeners to change the class of the squares on mouseover to create a
    drawing effect

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,29 +1,18 @@
-console.log("hello world");
-
 const container = document.querySelector("#container");
-console.log(container);
 
-for (let i = 0; i < 16; i++) {
+// create 16 x 16 grid of squares - 256 total squares
+for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement("div");
     squareDiv.setAttribute("class","square");
     container.appendChild(squareDiv);
-    console.log(i);
 }
 
-// event listeners
+// add event listeners to all squares to trigger on mouseover
 const squares = document.querySelectorAll(".square");
-console.log(squares);
 
-// for (let i = 0; i < squares.length; i++) {
-//     const currentSquare = squares[i];
-//     currentSquare.addEventListener("onclick", () => {
-//         currentSquare.setAttribute("class", "black-square");
-//         console.log("square " + i + " was clicked");
-//     });
-//     console.log("added event listener to square " + i);
-// }
-
-squares[0].addEventListener("onclick", (event) => {
-    event.target.setAttribute("class", "black-square");
-    console.log("square 0 was clicked");
-});
+for (let i = 0; i < squares.length; i++) {
+    let currentSquare = squares[i];
+    currentSquare.addEventListener("mouseover", (event) => {
+        event.target.setAttribute("class", "black-square");
+    });
+}
